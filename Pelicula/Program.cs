773341b60150b7837c821dce88db19bb56bc856b
LuisRoldanC <L22211648@tectijuana.edu.mx>
2023-03-20 16:56:12 -0700

    Quinta version: Incluye la clase actores y metodos necesarios para imprimir el contenido del main.

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -6,105 +6,144 @@ namespace Pelicula
 
     public class Pelicula
     {
-    //Campos
-        private string titulo;
-        private Int16 año;
-        private string pais;
-        private string director;
+        //Campos
+        public string titulo;
+        public Int16 año;
+       // public string pais;
+       // public string director;
+        private List<Actor> actores = new List<Actor>();
 
-      //  private List<Actor> actores = new List<Actor>();
         //Constructores
-     
-      //Métodos
+         //Métodos
   /*  public Pelicula (string titulo, Int16 año, string pais, string director){
         this.titulo = titulo;
         this.año = año;
         this.pais = pais;
         this.director = director;
 }*/
-
-        public void SetTitulo(string titulo)
-        {
-            this.titulo = titulo;
-        }
-        public void SetAño(Int16 año)
-        {
-            this.año = año;
-        }
-        public string GetTitulo()
-        {
-            return titulo;
-        }
-        public string Titulo {
-            get { return titulo; }
-            set { titulo = value; }
-        }
-        public Int16 GetAño()
-        {
+        public Int16 GetAño(){
             return año;
+            }
+          public string GetTitulo(){
+            return titulo;
+            }
+        public void SetAño(Int16 año){
+            this.año = año;
+            }
+
+        public void SetTitulo(string Titulo){
+            this.Titulo = Titulo;}
+        public string Titulo { 
+            get {return titulo;}
+            set {titulo = value;}
+            }
+         public Int16 Año { 
+            get {return año;}
+            set {año = value;}
+            }  
+         public Pelicula(string Titulo, Int16 Año) {
+            this.Titulo = Titulo;
+            this.Año = Año;
+            }
+        public Pelicula() {
+
+            }
+        public void Imprime() {
+          Console.WriteLine($"{titulo} ({año})");
+            }
+            public void ImprimeActores(){
+
+            foreach (Actor a in actores)
+             {
+               Console.WriteLine(a);
+            }
+                }
+            public void AgregaActor(Actor actor) {
+            actores.Add(actor);
+            }
         }
-        public Int16 Año {
-            get { return año; }
-            set { año = value; }
-        }
-        public Pelicula(string t, Int16 a){
-            titulo = t;
-            año = a;
-        }
-        public Pelicula(){
-        }
-            public void Imprime()
-        {
-        Console.WriteLine($"{titulo} ({año})");
-        }
-    }
+        
+
+       
 
 
-
+    // Puedes probar tu código en Main() pero lo importante
+    // es que pase las pruebas
+    
     public class Actor
     {
         //Propiedades
+        private string NombreA;
+        private Int16 AñoA; 
 
         //Constructores
-
+        public Actor(string NombreA, Int16 AñoA) {
+            this.NombreA = NombreA;
+            this.AñoA = AñoA;
+        }
 
         //Métodos 
-        public void Imprime()
-        {
-          //  Console.WriteLine($"{Nombre} ({Año})");
+        public override string ToString(){
+            return $"{NombreA} ({AñoA})";
+        }  
+        public Int16 GetAñoActor(){
+            return AñoA;
+            }
+        public string GetNombre(){
+            return NombreA;
+            }
+        public void SetAñoActor(Int16 añoA){
+            this.AñoA = añoA;
+            }
+     
+        public void SetNombre(string NombreA){
+            this.NombreA = NombreA;
+            }
+        public string nombreA { 
+            get {return NombreA;}
+            set {NombreA = value;}
+        }
+        public Int16 añoA { 
+            get {return AñoA;}
+            set {AñoA = value;}
         }
     }
 
     // Puedes probar tu código en Main() pero lo importante
     // es que pase las pruebas
-
+    
     class Program
     {
         static void Main(string[] args)
-        {
-       List<Pelicula> peliculas = new List<Pelicula>();
+        { 
 
-        peliculas.Add(new Pelicula () {Titulo = "Titanic", Año = 1997});
-        peliculas.Add(new Pelicula () {Titulo = "Pinocho", Año = 2022});
-        peliculas.Add(new Pelicula () {Titulo = "Rapidos y furiosos 10", Año = 2023});   
-        peliculas.Add(new Pelicula () {Titulo = "Top Gun Maverick", Año = 2021});
-        peliculas.Add(new Pelicula () {Titulo = "El Rey Leon", Año = 2020});
-
+        
+        Pelicula p1 = new Pelicula("La La Land", 2016);
+        p1.AgregaActor(new Actor("Ryan Gosling", 1980));
+        p1.AgregaActor(new Actor("Emma Stone", 1988));
+        p1.ImprimeActores();
+            /*
+        List<Pelicula> peliculas = new List<Pelicula>();
+        
+        peliculas.Add(new Pelicula() { Titulo = "La La Land", año = 2016 });
+        peliculas.Add(new Pelicula() { Titulo = "Green Book", año = 2019 });
+        peliculas.Add(new Pelicula() { Titulo = "Parasite", año = 2019 });
+        peliculas.Add(new Pelicula() { Titulo = "Pinocho", año = 2022 });
+        peliculas.Add(new Pelicula() { Titulo = "CODA", año = 2021 });
         foreach (Pelicula p in peliculas)
         {
-            Console.WriteLine("{0} ({1})", p.Titulo, p.Año);
-        }
-
-        /*
-        Pelicula p1 = new Pelicula();
-        p1.SetTitulo("Titanic");
-        p1.SetAño(1997);
-        Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
-        Pelicula p2 = new Pelicula();
-        p2.SetTitulo("Pinocho");
-        p2.SetAño(2022);
-        Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
-       */
+            Console.WriteLine($"{p.Titulo} ({p.año})");
+        } 
+           
+           Pelicula p1 = new Pelicula();
+           p1.SetTitulo("La La Land");
+           p1.SetAño(2016);
+           Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+           
+           Pelicula p1 = new Pelicula("Parasitos","Corea del Sur",2019,"Bong Joon-ho");
+           p1.Imprime();
+           Pelicula p2 = new Pelicula("Green Book","EUA",2019,"Peter Farrelly");
+           p2.Imprime(); */
         }
     }
 }
